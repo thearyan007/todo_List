6db77c4b5a0f200d594e43c270e0eb713e7317f4
thearyan007 <thearyangupta007@gmail.com>
2023-08-24 23:05:13 +0530

    added script

---
+++ script.js
@@ -0,0 +1,26 @@
+const inputBox= document.getElementById("input-box") ;
+const listContainer = document.getElementById("list-cointainer");
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("add something in the todoList!")
+    }else{
+        let li=document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span= document.createElement("span");
+        span.innerHTML= "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value= "";
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName=== "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+const numOfLis = document.querySelectorAll("li").length;
