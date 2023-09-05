b8a69d464c2cabdda4ee7a42cf8ca9c5ec118e38
thearyan007 <thearyangupta007@gmail.com>
2023-09-05 19:11:17 +0530

    final

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const inputBox= document.getElementById("input-box") ;
 const listContainer = document.getElementById("list-cointainer");
+const tasks= document.getElementsByClassName("tasks");
 
+var totalTask;
+var taskCompleted=0;
 function addTask(){
     if(inputBox.value===''){
         alert("add something in the todoList!")
@@ -12,22 +15,41 @@ function addTask(){
         span.innerHTML= "\u00d7"
         li.appendChild(span);
     }
+    updatecounter()
     inputBox.value= "";
 }
+
+function updatecounter(){
+    totalTask= listContainer.getElementsByTagName("li").length;
+    taskCompleted= listContainer.getElementsByClassName("checked").length;
+
+    // p.innerText = "You have completed " + taskCompleted + " out of " + totalTask;
+    document.querySelector("p").innerHTML=`You have completed  ${taskCompleted} out of ${totalTask}`;
+
+
+    console.log(taskCompleted+ " "+ totalTask);
+}
+
+
 
 inputBox.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
         addTask();
+        updatecounter();
     }
 });
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        
     }
     else if(e.target.tagName=== "SPAN"){
         e.target.parentElement.remove();
+
     }
+    updatecounter();
 }, false);
 const numOfLis = document.querySelectorAll("li").length;
+
